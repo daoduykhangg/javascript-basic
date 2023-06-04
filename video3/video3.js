ad8be0eafae3ac30d545d7f29ad3b72ffa186490
Khang Dao <khang.daoduy@yum.com>
2023-06-04 15:02:41 +0700

    update code

---
+++ video3/video3.js
@@ -0,0 +1,4 @@
+let a = 5;
+let b = 5.9;
+const x = 5;
+console.log('Hello world from JS', x);
---
+++ video4/video4.js
@@ -0,0 +1,5 @@
+let a = `I'm String - String`;
+let text = `toi ten name`;
+console.log(a);
+console.log(a.length);
+console.log(a.length);
